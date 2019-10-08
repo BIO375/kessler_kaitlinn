7266ed12e930d6347c52d8d381d1af050208397b
kaitlinnkessler <kaitlinkessler_2021@depauw.edu>
2019-10-08 12:45:51 -0400

    lab 5

--- Assignments/T-test_2019-09-24.R
+++ Assignments/T-test_2019-09-24.R
@@ -7,3 +7,30 @@ getwd()
 library("tidyverse")
 # Check for updates
 tidyverse_update()
+library(readr)
+
+birth_rate_2019_09_24 <- read_csv("datasets/demos/birth_rate_2019-09-24.csv")
+View(birth_rate_2019_09_24)
+
+summary(birth_rate_2019_09_24)
+
+ggplot(birth_rate_2019_09_24)+
+  geom_histogram(aes(Birth_diff), bindwith = 10) 
+ggplot(birth_rate_2019_09_24)+
+  geom_histogram(aes(Birth_diff), bindwith = 30)
+
+ggplot(birth_rate_2019_09_24)+
+  geom_boxplot(aes(x = Country, y = Birth_diff), notch = TRUE, varwidth = TRUE)
+
+data01 <- read_csv("datasets/abd/chapter12/chap12e3HornedLizards.csv")
+data01 <- data01 %>% slice (-105)
+
+summary(data01)
+
+ggplot(data01)+
+  geom_histogram(aes(squamosalHornLength), bindwith= 10)
+
+ggplot(data01)+
+  geom_boxplot(aes(x = Survival, y = squamosalHornLength), notch = TRUE, varwidth = TRUE)
+
+
